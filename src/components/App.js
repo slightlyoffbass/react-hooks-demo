8f3809c6906eb26fefcc3c661039e96c15d5df3e
jeff.smith <slightlyoffbass@gmail.com>
2019-10-08 13:00:28 -0500

    basic project

---
+++ src/components/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+class App extends React.Component {
+  
+  state = {resource: 'posts'};
+  
+  render(){
+    return (
+      <div className="ui container">
+        <div>
+          <button 
+            onClick={()=> this.setState({resource: 'posts'})}>
+              Posts
+          </button>
+          <button
+            onClick={()=> this.setState({resource: 'todos'})}>
+              ToDos
+          </button>
+        </div>
+        {this.state.resource}
+      </div>
+     );
+  }
+  
+
+}
+
+export default App;
