010d4c35425afe6b6dec3fed7852cc4f6a4e81d6
jeff.smith <slightlyoffbass@gmail.com>
2019-10-08 16:18:02 -0500

    end of day push

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ResourceList from '../components/ResourceList';
 
-class App extends React.Component {
+const App = () => {
   
-  state = {resource: 'posts'};
-  
-  render(){
-    return (
-      <div className="ui container">
-        <div>
-          <button 
-            onClick={()=> this.setState({resource: 'posts'})}>
-              Posts
-          </button>
-          <button
-            onClick={()=> this.setState({resource: 'todos'})}>
-              ToDos
-          </button>
-        </div>
-        {this.state.resource}
+  const [resource, setResource] = useState('posts');
+
+  return (
+    <div className="ui container">
+      <div>
+        <button 
+          onClick={() => setResource( 'posts' )}>
+            Posts
+        </button>
+        <button
+          onClick={() => setResource( 'todos' )}>
+            ToDos
+        </button>
       </div>
-     );
-  }
-  
-
+      <ResourceList resource={resource}/>
+    </div>
+    );
 }
 
 export default App;
